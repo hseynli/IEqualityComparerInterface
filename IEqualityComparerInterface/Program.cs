2dc2663c832c6d3b89b18a6c5a372e9d965fea81
Farid <hseynli@list.ru>
2024-06-18 14:11:13 +0400

    Initial commit

---
+++ IEqualityComparerInterface/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    public static void Main()
+    {
+        List<Person> list = new() 
+        {
+            new Person("John", "Doe"),
+            new Person("Jane", "Doe"),
+            new Person("John", "Smith"),
+            new Person("Jane", "Smith")
+        };
+
+        bool result = list.Contains(new Person("John", "Doe"));
+        Console.WriteLine(result);
+
+        Console.WriteLine("\nDone!");
+    }
+}
+
+// If we use record then the result must be True, otherwise False
+internal class Person(string FirstName, string LastName)
+{
+    public string FirstName { get; } = FirstName;
+    public string LastName { get; } = LastName;
+}
+
+internal class PersonEqualityComparer : IEqualityComparer<Person>
+{
+    public bool Equals(Person x, Person y) => x.FirstName == y.FirstName && x.LastName == y.LastName;
+
+    public int GetHashCode(Person obj) => obj.FirstName.GetHashCode() ^ obj.LastName.GetHashCode();
+}
---
+++ IEqualityComparerInterface/ProgramDictionary.cs
@@ -0,0 +1,61 @@
+﻿public class ProgramDictionary
+{
+    public static void Main_Dictionary()
+    {
+        BoxEqualityComparer comparer = new();
+
+        Dictionary<Box, string> boxes = new(comparer);
+
+        AddBox(new Box(4, 3, 4), "red");
+        AddBox(new Box(4, 3, 4), "blue");
+        AddBox(new Box(3, 4, 3), "green");
+
+        Console.WriteLine($"The dictionary contains {boxes.Count} Box objects.");
+
+        void AddBox(Box box, string name)
+        {
+            try
+            {
+                boxes.Add(box, name);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine($"Unable to add {box}: {e.Message}");
+            }
+        }
+    }
+}
+
+class Box
+{
+    public int Height { get; }
+    public int Length { get; }
+    public int Width { get; }
+
+    public Box(int height, int length, int width)
+    {
+        Height = height;
+        Length = length;
+        Width = width;
+    }
+
+    public override string ToString() => $"({Height}, {Length}, {Width})";
+}
+
+class BoxEqualityComparer : IEqualityComparer<Box>
+{
+    public bool Equals(Box? b1, Box? b2)
+    {
+        if (ReferenceEquals(b1, b2))
+            return true;
+
+        if (b2 is null || b1 is null)
+            return false;
+
+        return b1.Height == b2.Height
+            && b1.Length == b2.Length
+            && b1.Width == b2.Width;
+    }
+
+    public int GetHashCode(Box box) => box.Height ^ box.Length ^ box.Width;
+}
